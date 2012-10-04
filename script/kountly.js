e633400bf16a95393cf2e7ed9294425a38aebeff
sam <sam@pluto.aims-informatique.net>
2012-10-04 18:44:10 +0200

    ajout font

---
+++ script/kountly.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('html').css({
+	fontSize: $('html').height() * 16/920
+});
+$(window).resize(function() {
+	$('html').css({
+		fontSize: $('html').height() * 16/920
+	});
+});
+  
