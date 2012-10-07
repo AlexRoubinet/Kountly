670fa0057cde71e1fbf4b2cf0c90733e0476bcb7
sam <sam@Pluto.local>
2012-10-07 23:09:13 +0200

    home non percent

--- script/kountly.js
+++ script/kountly.js
@@ -1,12 +1,42 @@
 'use strict';
 
 $('html').css({
-	fontSize: $('html').height() * 16/920
+//	fontSize: $('html').height() * 16/920
 });
 $('app').width()
 $(window).resize(function() {
 	$('html').css({
-		fontSize: $('html').height() * 16/920
+//		fontSize: $('html').height() * 16/920
 	});
 });
   
+function Kountly(){
+	var self = this;
+	
+	function counter(){
+		this.counter = 0;
+		this.title = '';
+	}
+	
+	//Actions 
+	this.new_counter = function(){
+		alert('new counter');
+	};
+	this.counters = function(){
+		alert('new count');
+	};
+	
+	// controll
+	$('#app').find('a').on('click',function(e){
+		e.preventDefault();
+		console.log($(this).data('action'));
+		self[$(this).data('action')]();
+	});
+	
+	//view
+	
+	
+	
+}
+
+$(function(){new Kountly();});
