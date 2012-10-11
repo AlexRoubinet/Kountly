b27ed8f697020f4fcefaf4dee3535e423b6b239f
sam <sam@Pluto.local>
2012-10-11 23:37:00 +0200

    compteur date

--- script/kountly.js
+++ script/kountly.js
@@ -13,10 +13,11 @@ $(window).resize(function() {
 function Kountly(){
 	var self = this;
 	
-	function Counter(title){
+	function Counter(init){
 		this.key = (new Date()).getTime();
-		this.counter = 1;
-		this.title = title;
+		this.counter = init.counter;
+		this.title = init.title;
+		this.date = init.date;
 		this.save = function(){
 			localStorage.setItem(this.key,JSON.stringify(this));
 			self.counters.add(this.key);
@@ -26,11 +27,12 @@ function Kountly(){
 			$('#list').find('ul').append(
 				('<li>\
 					<b>1</b>\
-					<p>{titre}</p>\
+					<p>{title}</p>\
 					<small>{date}</small>\
 					<a href="#" data-action="add"></a>\
 				</li>'
-				).replace('{titre}',this.title)
+				).replace('{title}',this.title)
+				.replace('{date}',this.date)
 			);
 		};
 		return this;
@@ -46,7 +48,7 @@ function Kountly(){
 		
 		for(var i = 0;i< this.list.length; i++){
 			var e = JSON.parse(localStorage.getItem(this.list[i]));
-			var c = new Counter(e.title);
+			var c = new Counter(e);
 			
 			c.counter = e.counter;
 			c.render();
@@ -60,7 +62,11 @@ function Kountly(){
 	
 	//Actions 
 	this.new_counter = function(text){
-		var counter = new Counter(text);
+		var counter = new Counter({
+			title: text,
+			date: date_format(),
+			counter: 1	
+		});
 		counter.save();
 	};
 	
@@ -82,6 +88,15 @@ function Kountly(){
 	
 	
 	
+}
+
+function date_format(){
+	var d = new Date()
+		,days_of_week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+		,months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+	return days_of_week[d.getDay()] + ', ' 
+		+ months[d.getMonth()] + ' ' + d.getDate() + ' ' + d.getFullYear() 
+		+ ' at ' + d.getHours()%12 +':'+ d.getMinutes() + ' ' + (d.getHours()>11? 'p.m.' : 'a.m.');
 }
 
 $(function(){
